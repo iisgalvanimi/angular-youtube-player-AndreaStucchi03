8903a1375e913476b84a6ea2e26d8aaf9be749a4
AndreaStucchi03 <stucchi.andrea@iisgalvanimi.edu.it>
2022-02-24 08:43:24 +0000

    213-Esercizio Youtube Player-Stucchi

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule ,YouTubePlayerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
